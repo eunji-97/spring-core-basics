010b5b05701c6cc3426093827670ede6e92f5b82
eunji-97 <eunji.ko.dev@gmail.com>
2026-06-06 20:16:50 +0900

    43. 중복 등록과 충돌

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) //Configuration 애노테이션이 붙은건 제외하겠다 (충돌 방지)
 ) //자동으로 빈 등록!
 public class AutoAppConfig {
